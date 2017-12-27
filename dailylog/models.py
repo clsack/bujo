57a8056f824965d4c0f0b7ba8216e104345e3c0d
clsack <clsack@gmail.com>
2017-12-27 20:53:39 -0300

    modificaciones de templates y class meta

--- dailylog/models.py
+++ dailylog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,11 +9,14 @@ class Event(models.Model):
     date_from = models.DateTimeField()
     date_to = models.DateTimeField()
 
+    class Meta:
+        ordering = ['date_from']
+        get_latest_by = ['-date_from']
+
     def __str__(self):
         return self.event_text
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse('event-detail', kwargs={'pk': self.pk})
 
 
@@ -22,11 +26,14 @@ class Appointment(models.Model):
     date_to = models.DateTimeField()
     location = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ['date_from']
+        get_latest_by = ['-date_from']
+
     def __str__(self):
         return self.appointment_text
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse('appointment-detail', kwargs={'pk': self.pk})
 
 
@@ -36,11 +43,14 @@ class Task(models.Model):
     date_completed = models.DateField()
     completed = models.BooleanField()
 
+    class Meta:
+        ordering = ['date_scheduled']
+        get_latest_by = ['-date_scheduled']
+
     def __str__(self):
         return self.task_text
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse('task-detail', kwargs={'pk': self.pk})
 
 
@@ -48,11 +58,14 @@ class Deadline(models.Model):
     deadline_text = models.CharField(max_length=100, verbose_name='Description')
     date_scheduled = models.DateTimeField()
 
+    class Meta:
+        ordering = ['date_scheduled']
+        get_latest_by = ['-date_scheduled']
+
     def __str__(self):
         return self.deadline_text
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse('deadline-detail', kwargs={'pk': self.pk})
 
 
@@ -73,11 +86,14 @@ class Expense(models.Model):
     currency = models.CharField(max_length=3, choices=CURRENCY, default=ARS)
     paid = models.BooleanField()
 
+    class Meta:
+        ordering = ['date_scheduled']
+        get_latest_by = ['-date_scheduled']
+
     def __str__(self):
         return self.expense_text
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse('expense-detail', kwargs={'pk': self.pk})
 
 
@@ -86,9 +102,12 @@ class Note(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['date_modified']
+        get_latest_by = ['-date_modified']
+
     def __str__(self):
         return self.note_text
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse('note-detail', kwargs={'pk': self.pk})
--- dailylog/views.py
+++ dailylog/views.py
@@ -8,142 +8,172 @@ from .models import Event, Appointment, Task, Deadline, Expense, Note
 
 
 def index(request):
-    return render(request, 'dailylog/base.html')
+    return render(request, 'index.html')
 
 
 class EventCreate(CreateView):
     model = Event
     fields = ['event_text', 'date_from', 'date_to']
+    template_name = 'events/form.html'
 
 
 class EventUpdate(UpdateView):
     model = Event
     fields = ['event_text', 'date_from', 'date_to']
+    template_name = 'events/form.html'
 
 
 class EventDelete(DeleteView):
     model = Event
     success_url = reverse_lazy('index')
+    template_name = 'events/confirm_delete.html'
 
 
 class EventDetail(generic.DetailView):
     model = Event
+    template_name = 'events/detail.html'
 
 
 class EventListView(ListView):
     model = Event
+    template_name = 'events/list.html'
 
 
 class AppointmentCreate(CreateView):
     model = Appointment
     fields = ['appointment_text', 'date_from', 'date_to', 'location']
+    template_name = 'appointments/form.html'
 
 
 class AppointmentUpdate(UpdateView):
     model = Appointment
     fields = ['appointment_text', 'date_from', 'date_to', 'location']
+    template_name = 'appointments/form.html'
 
 
 class AppointmentDelete(DeleteView):
     model = Appointment
     success_url = reverse_lazy('index')
+    template_name = 'appointments/confirm_delete.html'
 
 
 class AppointmentDetail(generic.DetailView):
     model = Appointment
+    template_name = 'appointments/detail.html'
 
 
 class AppointmentListView(ListView):
     model = Appointment
+    template_name = 'appointments/list.html'
 
 
 class TaskCreate(CreateView):
     model = Task
     fields = ['task_text', 'date_scheduled', 'date_completed', 'completed']
+    template_name = 'tasks/form.html'
 
 
 class TaskUpdate(UpdateView):
     model = Task
     fields = ['task_text', 'date_scheduled', 'date_completed', 'completed']
+    template_name = 'tasks/form.html'
 
 
 class TaskDelete(DeleteView):
     model = Task
     success_url = reverse_lazy('index')
+    template_name = 'tasks/confirm_delete.html'
 
 
 class TaskDetail(generic.DetailView):
     model = Task
+    template_name = 'tasks/detail.html'
 
 
 class TaskListView(ListView):
     model = Task
+    template_name = 'tasks/list.html'
 
 
 class DeadlineCreate(CreateView):
     model = Deadline
     fields = ['deadline_text', 'date_scheduled']
+    template_name = 'deadlines/form.html'
 
 
 class DeadlineUpdate(UpdateView):
     model = Deadline
     fields = ['deadline_text', 'date_scheduled']
+    template_name = 'deadlines/form.html'
 
 
 class DeadlineDelete(DeleteView):
     model = Deadline
     success_url = reverse_lazy('index')
+    template_name = 'deadlines/confirm_delete.html'
 
 
 class DeadlineDetail(generic.DetailView):
     model = Deadline
+    template_name = 'deadlines/detail.html'
 
 
 class DeadlineListView(ListView):
     model = Deadline
+    template_name = 'deadlines/list.html'
 
 
 class ExpenseCreate(CreateView):
     model = Expense
     fields = ['expense_text', 'date_scheduled', 'amount', 'currency', 'paid']
+    template_name = 'expenses/form.html'
 
 
 class ExpenseUpdate(UpdateView):
     model = Expense
     fields = ['expense_text', 'date_scheduled', 'amount', 'currency', 'paid']
+    template_name = 'expenses/form.html'
 
 
 class ExpenseDelete(DeleteView):
     model = Expense
     success_url = reverse_lazy('index')
+    template_name = 'expenses/confirm_delete.html'
 
 
 class ExpenseDetail(generic.DetailView):
     model = Expense
+    template_name = 'expenses/detail.html'
 
 
 class ExpenseListView(ListView):
     model = Expense
+    template_name = 'expenses/list.html'
 
 
 class NoteCreate(CreateView):
     model = Note
     fields = ['note_text']
+    template_name = 'notes/form.html'
 
 
 class NoteUpdate(UpdateView):
     model = Note
     fields = ['note_text']
+    template_name = 'notes/form.html'
 
 
 class NoteDelete(DeleteView):
     model = Note
     success_url = reverse_lazy('index')
+    template_name = 'notes/confirm_delete.html'
 
 
 class NoteDetail(generic.DetailView):
     model = Note
+    template_name = 'notes/detail.html'
 
 
 class NoteListView(ListView):
     model = Note
+    template_name = 'notes/list.html'
